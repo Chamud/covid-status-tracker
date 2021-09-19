6e088ac4328cfcbfb6c206b1b780a548137bedc2
Chamud <chamudgeesara@gmail.com>
2021-09-19 11:17:21 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn covidapp.wsgi --log-file -
---
+++ covidapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for covidapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covidapp.settings')
+
+application = get_asgi_application()
