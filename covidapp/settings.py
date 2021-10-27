63777c0438afa3715216b5e45e3db871440cab41
Chamud <chamudgeesara@gmail.com>
2021-10-27 17:23:31 +0530

    Add postgres

--- covidapp/settings.py
+++ covidapp/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,7 +43,6 @@ INSTALLED_APPS = [
 
     'symptomtracker',
     'map',
-    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +86,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
