9c5ccdf00e3cdc8b71eb03f5fdb3e9994a846ae2
Chamud <chamudgeesara@gmail.com>
2021-12-03 23:14:32 +0530

    add home page

--- test.py
+++
@@ -1,8 +0,0 @@
-print('____________________________________________________________________________________________');
-import requests
-
-pload = {'username':'admin1','password':'5CPiu@gv5XYFSjU'}
-q = 'https://covidstatustracker.herokuapp.com/api/profile'
-p = 'http://127.0.0.1:8000/api/login'
-r = requests.get(q, data=pload)
-print(r.text)
